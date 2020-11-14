921a60584311e3729825af76689b8ea66921359e
xueshun <xueshun1211@163.com>
2020-11-14 17:02:59 +0800

    [add] share mem

---
+++ src/chapter17/share_mem/share_mem_test.go
@@ -0,0 +1,64 @@
+package share_mem
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+/*
+	不是并发并发控制
+*/
+func TestCounter(t *testing.T) {
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		go func() {
+			counter++
+		}()
+	}
+	time.Sleep(1 * time.Second)
+	t.Logf("counter = %d", counter)
+}
+
+/*
+	线程安全
+*/
+func TestCounterThreadSafe(t *testing.T) {
+	var mut sync.Mutex
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		go func() {
+			defer func() {
+				mut.Unlock()
+			}()
+			mut.Lock()
+			counter++
+		}()
+	}
+	time.Sleep(time.Second * 1)
+	t.Logf("counter = %d", counter)
+}
+
+/*
+	使用WaitGroup
+		上面线程安全代码，守护线程要不其他线程执行快，
+		需要等待其他线程执行完毕
+*/
+func TestCounterWaitGroup(t *testing.T) {
+	var mut sync.Mutex
+	var wg sync.WaitGroup
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		wg.Add(1)
+		go func() {
+			defer func() {
+				mut.Unlock()
+			}()
+			mut.Lock()
+			counter++
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	t.Logf("counter = %d", counter)
+}
